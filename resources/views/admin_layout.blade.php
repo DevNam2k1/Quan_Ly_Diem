e5baa11d0f0490daf9058d4acb5019fd0ee9b631
DevNam2k1 <dinhnamsaker@gmail.com>
2021-07-19 01:59:39 +0700

    Upload image

--- resources/views/admin_layout.blade.php
+++ resources/views/admin_layout.blade.php
@@ -391,6 +391,9 @@
     </section>
     <!-- /.content -->
   </div>
+  <a id="back-to-top" href="#" class="btn btn-primary back-to-top" role="button" aria-label="Scroll to top">
+    <i class="fas fa-chevron-up"></i>
+  </a>
   <!-- /.content-wrapper -->
   <footer class="main-footer">
     <strong>Copyright &copy; 2021 <a href="https://adminlte.io">Quản Lý Sinh Viên</a>.</strong>
